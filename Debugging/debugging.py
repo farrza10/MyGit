6f56e09de67706e9efc96840a434d49e04003529
farrza10 <70225671+farrza10@users.noreply.github.com>
2023-10-11 14:36:29 +0300

    Update debugging.py

--- Debugging/debugging.py
+++ Debugging/debugging.py
@@ -15,10 +15,14 @@ number1 = int(input("Enter Number"))
 result1 = function(number1)
 result2 = function(result1)
 
+import pdb; pdb.set_trace() ##adding for the sake of printing the second result2
+
 print(result2)
 
 1. python debugging.py
-2. check the result with 'step'
+2. check the end result of function with 'step'
+   2.1 using p result1 -> gives the result of resul1
+   2.2 using p result2 -> does not give since pdb.set_trace is before result2, adding after result2 for the sake of tracing
 3. check where you are in set_trace (shows all the codes) with 'l'
 4. step over the next line with 'n'
 5. then you can cgeck with 'l' to see if you moved to a line down
